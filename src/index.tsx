1d47a572e3367559bbe51b60f5c2417aa318cb0a
Roman Kovalchuk <roma220702@gmail.com>
2022-09-28 13:28:18 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import { store } from './app/store';
 
